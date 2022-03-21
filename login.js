a0062f5fe0c75535784f7e941d29f697ce2fd807
Robert Ribelin <rardev@simulacron-002.local>
2022-03-21 16:10:13 -0700

    added an html and css file for login. Working on login logic based on existing code

--- login.js
+++ login.js
@@ -1,7 +1,36 @@
-let enteredUserName = document.getElementById("username");
-let enteredEmail = document.getElementById("email");
-let password = document.getElementById("password");
+let userName;
+let password;
 
-async function 
+async function collectData()
+{
+    userName = document.getElementById("userName").value;
+    password = document.getElementById("password").value;
+    
+    const loginData = {userName, password};
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(loginData)
+    };
+    const response = await fetch('/api'/*'../server/api'*/, options);
+    const d = await response.json();
+    console.log(d);
+    console.log(loginData);
+    
+}
 
 
+function validateEmail(email)
+{
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+
+function confirmPassord()
+{   
+    if(password.length > 0)
+        return password == cPassword;
+    return false;
+}
