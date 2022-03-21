d5d73b02073cea8cae175acb568ee6750c78a398
Robert Ribelin <robert@ribelin.dev>
2022-03-21 11:55:36 -0700

    login page logic started based off of existing sign up code.

---
+++ login.js
@@ -0,0 +1,7 @@
+let enteredUserName = document.getElementById("username");
+let enteredEmail = document.getElementById("email");
+let password = document.getElementById("password");
+
+async function 
+
+
